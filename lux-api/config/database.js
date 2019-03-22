5dd3bf3393fa6850b5d97c0de883e6a922cf3577
jame mackson <jame.mackson@gmail.com>
2019-03-22 15:20:36 -0500

    database and CORS config

--- lux-api/config/database.js
+++ lux-api/config/database.js
@@ -1,7 +1,20 @@
+// get connection props from env
+const {
+  DB_HOST = 'localhost',
+  DB_PORT = '5432',
+  DB_USER = 'postgres',
+  DB_PASS = '',
+  DB_NAME = 'api_dev'
+} = process.env;
+
 export default {
   development: {
-    driver: 'sqlite3',
-    database: 'lux_api_dev'
+    driver: 'pg',
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USER,
+    password: DB_PASS
   },
 
   test: {
--- lux-api/config/environments/development.js
+++ lux-api/config/environments/development.js
@@ -1,9 +1,30 @@
 export default {
+  server: {
+    cors: {
+      origin: '*',
+      enabled: true,
+
+      headers: [
+        'Accept',
+        'Content-Type',
+        'Authorization'
+      ],
+
+      methods: [
+        'GET',
+        'POST',
+        'PATCH',
+        'DELETE',
+        'HEAD',
+        'OPTIONS'
+      ]
+    }
+  },
+
   logging: {
     level: 'DEBUG',
     format: 'text',
     enabled: true,
-
     filter: {
       params: []
     }
